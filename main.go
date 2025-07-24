fa9bab94cede605feb8f624078f1d2ff20df594a
Abuhurrara <abuhurrara1093@gmail.com>
2025-07-24 19:03:03 +0500

    Initial Commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import "net/http"
+
+type server struct {
+	address string
+}
+
+func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from the server!"))
+}
+
+func main() {
+	s := &server{address: ":8080"}
+	if err := http.ListenAndServe(s.address, s); err != nil {
+		panic(err)
+	}
+}
